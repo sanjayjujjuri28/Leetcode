a9e0449b345454a6d48ad1e14c26eeb4741f2bba
Sanjay Jujjuri <sanjayjujjuri9@gmail.com>
2026-08-19 21:17:45 +0530

    Time: 36 ms (34.44%), Space: 48.2 MB (9.5%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -8,17 +8,12 @@ class Solution {
             char ch=s.charAt(r);
             if(map.containsKey(ch)){
                int end=map.get(ch);
-               while(l<=end){
-                  map.remove(s.charAt(l));
-                  l++;
-               }
-               map.put(ch,r);
+               l=Math.max(l,end+1);
             }
-            else{
+        
                 map.put(ch,r);
                 maxLen=Math.max(maxLen,r-l+1);
-            }
-            r++;
+                r++;
         }
         return maxLen;
     }
